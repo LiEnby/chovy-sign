fd203bb4408cc6b234692f77dfd30461d5884736
Li <li@silica.codes>
2026-06-04 10:38:47 +1200

    make !CreatePsvImg psp output directory consistent

--- LibChovy/PspParameters.cs
+++ LibChovy/PspParameters.cs
@@ -14,6 +14,7 @@ namespace LibChovy
         {
             get
             {
+                if (!CreatePsvImg) return outputFolder;
                 return Path.Combine(outputFolder, Umd.DiscId);
             }
             set
